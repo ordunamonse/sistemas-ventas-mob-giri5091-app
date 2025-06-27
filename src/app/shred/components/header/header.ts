39068a0fce859e60869812785c0ed336cf79b4f6
ordunamonse <ordunamonse28@gmail.com>
2025-06-26 20:52:58 -0600

    estilos

--- src/app/shred/components/header/header.ts
+++ src/app/shred/components/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router  } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss'
 })
 export class Header {
+  constructor(private router: Router){}
 
+  navegarLogin(){
+    this.router.navigate(['/auth']);
+  }
 }
+
+
 
